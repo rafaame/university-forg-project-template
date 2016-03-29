2ff63600f4e29ee6e27bad4dbfc815ebf910c847
Rafael Andreatta <rafaame@Rafaels-iMac.local>
2016-03-29 13:55:38 -0300

    Initial commit

---
+++ include/common.h
@@ -0,0 +1,7 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+#include <string.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+
+	printf("Hello world\n");
+
+	return 0;
+
+}
